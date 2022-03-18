ea307fcdf22db3e952965d4534c3ad22da65398c
40detectives <9024463+40detectives@users.noreply.github.com>
2022-03-18 23:20:22 +0100

    chore: first hook example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier"></Pet>
+      <SearchParams />
     </div>
   );
 };
