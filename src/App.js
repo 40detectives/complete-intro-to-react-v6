cd4a3ee8679ba4189fcfbd32123e8d7d965be331
40detectives <9024463+40detectives@users.noreply.github.com>
2022-03-18 22:58:35 +0100

    chore: keep only the new react + jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
-/*
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    // ...[1, 2, 3, 4].map((x) => React.createElement("h1", {}, x)),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
-};
-*/
-
 const App = () => {
   return (
     <div>
