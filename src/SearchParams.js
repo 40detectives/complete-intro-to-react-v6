3c1bc68f5e9c4940f3d32a485bb1086c5d57a1df
40detectives <9024463+40detectives@users.noreply.github.com>
2022-03-19 19:19:19 +0100

    chore: add 2 more hooks for other inputs in the search form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -13,8 +18,38 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
-          <button>Submit</button>
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option value="" /> {/* empty initial option */}
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option value="" /> {/* empty initial option */}
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
       </form>
     </div>
   );
