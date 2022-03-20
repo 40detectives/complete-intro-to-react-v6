a9a9e801a59461232e3c668567fbe436ac54e414
40detectives <9024463+40detectives@users.noreply.github.com>
2022-03-20 18:45:21 +0100

    chore: add router to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1 id="my-brand">Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
