b443d17cec186c919d0c460181019953c0960b42
40detectives <9024463+40detectives@users.noreply.github.com>
2022-03-20 18:47:33 +0100

    chore: first class component

---
+++ src/Details.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await response.json();
+    this.setState({
+      loading: false,
+      ...json.pets[0], // the rest of the parameters from the API
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h1>{`${animal} - ${breed} - ${city},  ${state}`}</h1>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
